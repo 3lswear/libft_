e70c8a2f922103ff46b310cd2a5f1c2fa79fb272
3lswear <r.malyavsky@protonmail.com>
2020-11-10 18:10:03 +0300

    prototypes added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 12:33:06 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/10 12:37:53 by roman            ###   ########.fr       */
+/*   Updated: 2020/11/10 17:38:41 by roman            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr_fd(char *str, int fd);
+int		ft_atoi(char *str);
+int 	ft_isalpha(int c);
+int		ft_isdigit(int c);
+int 	ft_isalnum(int c);
+int 	ft_isascii(int c);
 
 #endif
-
