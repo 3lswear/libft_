debc4c7757129ebf4bbaae3cbcfbfb40382b6b6c
3lswear <r.malyavsky@protonmail.com>
2020-11-12 13:15:53 +0300

    removed strcmp, added strncmp, strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 12:33:06 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/11 15:35:18 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/11 18:50:51 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 
 int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
 void	ft_putstr_fd(char *str, int fd);
 int		ft_atoi(char *str);
 int 	ft_isalpha(int c);
@@ -26,5 +26,6 @@ int 	ft_isascii(int c);
 char	*ft_strchr_bd(char *s, int c, int dir);
 char	*ft_strchr(char *s, int c);
 char	*ft_strrchr(char *s, int c);
+char	*ft_strnstr(char *haystack, char* needle, int len);
 
 #endif
