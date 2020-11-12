9a497de0acf93fe8243fb7ccb0a613475f544473
3lswear <r.malyavsky@protonmail.com>
2020-11-12 13:30:52 +0300

    normified

--- libft.h
+++ libft.h
@@ -6,12 +6,12 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 12:33:06 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/11 18:50:51 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/12 13:30:18 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
 # include <unistd.h>
 
@@ -19,13 +19,13 @@ int		ft_strlen(char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
 void	ft_putstr_fd(char *str, int fd);
 int		ft_atoi(char *str);
-int 	ft_isalpha(int c);
+int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int 	ft_isalnum(int c);
-int 	ft_isascii(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
 char	*ft_strchr_bd(char *s, int c, int dir);
 char	*ft_strchr(char *s, int c);
 char	*ft_strrchr(char *s, int c);
-char	*ft_strnstr(char *haystack, char* needle, int len);
+char	*ft_strnstr(char *haystack, char *needle, int len);
 
 #endif
