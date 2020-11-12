b745b53a8a37d46c5b05b71137b40e1f06bab51c
3lswear <r.malyavsky@protonmail.com>
2020-11-12 13:16:07 +0300

    init

---
+++ ft_strnstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/11 18:20:10 by sunderle          #+#    #+#             */
+/*   Updated: 2020/11/12 13:13:00 by sunderle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(char *haystack, char *needle, int len)
+{
+	int n_len;
+	int i;
+
+	n_len = ft_strlen(needle);
+	i = 0;
+	while (i < len)
+	{
+		if ((!ft_strncmp(&haystack[i], needle, n_len)) && ((i + n_len) < len))
+			return (&haystack[i]);
+		else
+			i++;
+	}
+	return (0);
+}
