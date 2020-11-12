e13e8f524f014db924e8278524d51e2a2c59a3c6
3lswear <r.malyavsky@protonmail.com>
2020-11-12 13:13:45 +0300

    now using ft_isdigit

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi_strict.c                                   :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:00:27 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/10 15:25:56 by roman            ###   ########.fr       */
+/*   Updated: 2020/11/12 13:10:46 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	isanum(char *chr)
-{
-	if (*chr < '0' || *chr > '9')
-		return (0);
-	return (1);
-}
 
 int	ft_atoi(char *str)
 {
 	int		result;
 
 	result = 0;
-	while (isanum(str))
+	while (ft_isdigit(*str))
 	{
 		result = result * 10 + (*str - '0');
 		str++;
