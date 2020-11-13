93c9418c045495e0a4b70183e73195a2c60092af
3lswear <r.malyavsky@protonmail.com>
2020-11-13 04:27:48 +0300

    init preliminary

---
+++ ft_memmove.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/13 03:07:59 by sunderle          #+#    #+#             */
+/*   Updated: 2020/11/13 04:25:37 by sunderle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memmove(void *dest, void *src, unsigned int n)
+{
+	if (dest > src)
+		return (ft_memcpy_bd(dest, src, n, 1));
+	else if (src > dest)
+		return (ft_memcpy_bd(dest, src, n, 0));
+	else return (dest);
+}
