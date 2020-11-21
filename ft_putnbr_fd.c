2ac00bde2308af688c7325f30b8a30f6e17f489e
3lswear <r.malyavsky@protonmail.com>
2020-11-22 02:09:32 +0300

    removed redundant code, prettified

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 15:54:33 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/22 01:54:11 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/22 02:05:07 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,21 @@ int		get_round_dec(int n)
 	return (decimal);
 }
 
-void	loop(int numb, int fd)
+void	ft_putnbr_fd(int num, int fd)
 {
-	char	c;
+	char	digit;
 	int		power;
 
-	power = get_round_dec(numb);
+	power = get_round_dec(num);
+	if (num < 0)
+		ft_putchar_fd('-', fd);
 	while (power >= 1)
 	{
-		c = numb / power;
-		if (numb < 0)
-			c = -c;
-		c += '0';
-		ft_putchar_fd(c, fd);
-		numb = numb - (power * (numb / power));
-		power = power / 10;
+		digit = num / power;
+		if (num < 0)
+			digit = -digit;
+		ft_putchar_fd(digit + '0', fd);
+		num -= (power * (num / power));
+		power /= 10;
 	}
-}
-
-void	ft_putnbr_fd(int numb, int fd)
-{
-	int		power;
-	char	c;
-
-	power = get_round_dec(numb);
-	if (numb < 0)
-		ft_putchar_fd('-', fd);
-	if (get_round_dec(power) == 1)
-	{
-		c = numb;
-		if (numb < 0)
-			c = -c;
-		c += '0';
-		ft_putchar_fd(c, fd);
-	}
-	else
-		loop(numb, fd);
 }
