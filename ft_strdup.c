be48f242bf4ffd75c1b36fc22eeb4f62a560303f
3lswear <r.malyavsky@protonmail.com>
2020-11-23 17:35:41 +0300

    changed types, added nullptr check

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,17 +6,20 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 13:57:04 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/21 20:57:23 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/23 12:42:10 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strdup(char *src)
 {
-	char			*dest;
-	unsigned int	size;
+	char	*dest;
+	size_t	size;
 
 	size = ft_strlen(src) + 1;
 	dest = (char *)malloc(size * sizeof(char));
-	ft_strlcpy(dest, src, size);
+	if (dest)
+		ft_strlcpy(dest, src, size);
 	return (dest);
 }
