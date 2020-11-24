fb10d4213851056a123e72e2280bf93a069a3ca6
3lswear <r.malyavsky@protonmail.com>
2020-11-24 04:17:37 +0300

    char -> const char

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,13 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 13:57:04 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/23 12:42:10 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/24 00:15:41 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *src)
 {
 	char	*dest;
 	size_t	size;
