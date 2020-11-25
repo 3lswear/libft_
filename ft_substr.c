9a2961ad781e31f4fadf7e880fe4bafed763980c
3lswear <r.malyavsky@protonmail.com>
2020-11-26 01:53:23 +0300

    fixed logic

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 12:03:02 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/23 12:44:43 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 15:20:06 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *result;
 
-	if (start >= ft_strlen((char *)s))
+	if (!s)
 		return (NULL);
-	if (ft_strlen((char *)&s[start]) < len)
-		len = ft_strlen((char *)&s[start]);
+	if (ft_strlen(&s[start]) < len)
+		len = ft_strlen(&s[start]);
+	else if (start >= ft_strlen(s))
+		len = 0;
 	result = (char *)malloc((len + 1) * sizeof(char));
 	if (result)
 		ft_strlcpy(result, (char *)&s[start], len + 1);
