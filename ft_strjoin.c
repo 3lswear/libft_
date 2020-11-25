aefb2a50ffcdb1d58bad1e100d0bc74d91e030fd
3lswear <r.malyavsky@protonmail.com>
2020-11-26 01:42:49 +0300

    added nullptr check for both params

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 12:50:07 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/23 13:06:25 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 15:58:34 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 	char	*result;
 
+	if (!s1 || !s2)
+		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	result = (char *)malloc((len + 1) * sizeof(char));
 	if (result)
