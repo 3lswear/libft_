55ee78bbada8e62f94e64fa00370220f679a66c0
3lswear <r.malyavsky@protonmail.com>
2020-11-26 01:43:20 +0300

    added size and nullptr checks

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/15 17:17:10 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/24 00:17:43 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 15:30:34 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	unsigned int	src_len;
-	unsigned int	i;
+	size_t	src_len;
+	size_t	i;
 
+	if (!dest || !src)
+		return (0);
 	src_len = ft_strlen(src);
 	i = 0;
-	while ((i < size - 1) && (i < src_len))
-	{
-		dest[i] = src[i];
-		i++;
-	}
 	if (size)
+	{
+		while ((i < size - 1) && (i < src_len))
+		{
+			dest[i] = src[i];
+			i++;
+		}
 		dest[i] = '\0';
+	}
 	return (src_len);
 }
