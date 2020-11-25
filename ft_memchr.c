53b5eece8c0410ccf126a2823a7f28315e90e0a1
3lswear <r.malyavsky@protonmail.com>
2020-11-26 01:39:56 +0300

    cast arg to unsigned char

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 04:38:31 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/25 12:40:40 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 14:23:11 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	i = 0;
 	while (i++ < n)
-		if (((unsigned char *)s)[i - 1] == c)
+		if (((unsigned char *)s)[i - 1] == (unsigned char)c)
 			return (&((unsigned char *)s)[i - 1]);
 	return (NULL);
 }
