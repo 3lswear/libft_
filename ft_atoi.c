3301d50e07bd4be533329f31c6639afb44295855
3lswear <r.malyavsky@protonmail.com>
2020-11-25 13:48:56 +0300

    fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,42 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:00:27 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/24 04:28:03 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 12:02:21 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *str)
+int	ft_isspace(const char *chr)
 {
-	int		result;
+	if (*chr == '\n' || *chr == '\v' || *chr == '\f' ||
+			*chr == '\r' || *chr == ' ' || *chr == '\t')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int is_negative;
+	int result;
 
 	result = 0;
+	is_negative = 0;
+	while (ft_isspace(str))
+		str++;
+	if (*str == '-')
+	{
+		is_negative = 1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
 	while (ft_isdigit(*str))
 	{
 		result = result * 10 + (*str - '0');
 		str++;
 	}
+	if (is_negative)
+		result = -result;
 	return (result);
 }
