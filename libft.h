99bbdcf0df9ac11e419e72f64834b717aa4f2d4e
3lswear <r.malyavsky@protonmail.com>
2020-11-25 13:48:13 +0300

    fixed types, added memccpy

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sunderle <marvin@43.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 12:33:06 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/24 01:34:07 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/25 12:44:27 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int			ft_atoi(char *str);
+int			ft_atoi(const char *str);
 char		*ft_itoa(int n);
 
 size_t		ft_strlen(const char *str);
@@ -50,10 +50,11 @@ int			ft_tolower(int c);
 
 void		*ft_memset(void *s, int c, unsigned int n);
 void		ft_bzero(void *s, unsigned int n);
-void		*ft_memmove(void *dest, void *src, unsigned int n);
-void		*ft_memcpy_bd(void *dest, void *src, unsigned int n, int dir);
-void		*ft_memcpy(void *dest, void *src, unsigned int n);
-void		*ft_memchr(void *s, int c, unsigned int n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memcpy_bd(void *dest, const void *src, size_t n, int dir);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(void *s1, void *s2, unsigned int n);
 void		*ft_calloc(size_t nmemb, size_t size);
 
