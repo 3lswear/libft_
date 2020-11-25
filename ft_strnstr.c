22ce001c57136ffe517e564e878f6178de087a2b
3lswear <r.malyavsky@protonmail.com>
2020-11-26 01:51:38 +0300

    fixed types, added checks

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,25 +6,27 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 18:20:10 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/24 04:30:08 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/26 01:48:38 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(char *haystack, char *needle, int len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int n_len;
-	int i;
+	size_t n_len;
+	size_t i;
 
 	n_len = ft_strlen(needle);
+	if (haystack == needle || !n_len)
+		return ((char *)haystack);
 	i = 0;
 	while (i < len)
 	{
 		if ((!ft_strncmp(&haystack[i], needle, n_len)) && ((i + n_len) < len))
-			return (&haystack[i]);
+			return (&((char *)haystack)[i]);
 		else
 			i++;
 	}
-	return (0);
+	return (NULL);
 }
