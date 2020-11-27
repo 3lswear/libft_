ad9c3c2db3006bf4031b8b2101fd4b03a96671f2
3lswear <r.malyavsky@protonmail.com>
2020-11-27 16:20:25 +0300

    fixed possible segfault

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 12:03:02 by sunderle          #+#    #+#             */
-/*   Updated: 2020/11/25 15:20:06 by sunderle         ###   ########.fr       */
+/*   Updated: 2020/11/27 16:07:39 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(&s[start]) < len)
-		len = ft_strlen(&s[start]);
-	else if (start >= ft_strlen(s))
+	if (start >= ft_strlen(s))
 		len = 0;
-	result = (char *)malloc((len + 1) * sizeof(char));
-	if (result)
-		ft_strlcpy(result, (char *)&s[start], len + 1);
+	else if (ft_strlen(&s[start]) < len)
+		len = ft_strlen(&s[start]);
+	result = (char *)ft_calloc((len + 1), sizeof(char));
+	if (!len && result)
+		return (result);
+	else if (result)
+		ft_memcpy(result, (char *)&s[start], len);
 	return (result);
 }
